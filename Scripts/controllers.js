76b43b1f7050aef99c806fe5d0ca83955bb7d795
wangdm0824 <853383690@qq.com>
2015-01-27 14:07:17 +0800

    1

---
+++ Scripts/controllers.js
@@ -0,0 +1,27 @@
+﻿
+function OrgList($scope) {
+
+    $scope.Schools = [{
+        "name": "大智学校",
+        "snippet": "大智学校123"
+    }, {
+        "name": "齐鲁医院",
+        "snippet": "齐鲁医院123"
+    }, {
+        "name": "东方豪客",
+        "snippet": "东方豪客123"
+    }];
+
+    $scope.Courses = [{
+        "id": "1",
+        "name": "初中"
+    }, {
+        "id": "2",
+        "name": "高中"
+    }, {
+        "id": "3",
+        "name": "大学"
+    }];
+
+    $scope.Title = "AngularJS";
+}
