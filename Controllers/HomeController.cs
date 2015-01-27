418071af5e0af7e29a98b1538608c46c60975001
wangdm0824 <853383690@qq.com>
2015-01-27 09:55:15 +0800

    1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,6 +13,13 @@ namespace Test.Controllers
 
         public ActionResult Index()
         {
+            var data = new[] { 
+                              new {ID=1,Name="1"},
+                              new {ID=2,Name="2"},
+                              new {ID=3,Name="3"},
+                              new {ID=4,Name="4"}
+            };
+            //var result = Map(x => x.ID, data);         //高阶函数Map Filter Fold
             return View();
         }
 
